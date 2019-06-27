a3460ee5e8fbd90db02efe4e60b35291a9205b80
Lucas Zeer <lucas.zeer@gmail.com>
2019-06-27 10:55:05 -0400

    Update pathSetup.js
    Without PWD on NODE_PATH this library breaks w/ yarn workspaces

--- pathSetup.js
+++ pathSetup.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 
-module.exports = function(filePath) {
+module.exports = function(filePath, includePWD) {
   var orig, p, root, stat;
   p = filePath || path.join(__filename, '../../');
   stat = fs.lstatSync(p);
@@ -14,6 +14,9 @@ module.exports = function(filePath) {
     process.env.NODE_PATH = path.join(root, '../libs') + path.delimiter + orig;
   } else {
     process.env.NODE_PATH = root + path.delimiter + orig;
+  }
+  if (includePWD) {
+    process.env.NODE_PATH = process.env.PWD + ":" + process.env.NODE_PATH;
   }
   return require('module')._initPaths();
 };
